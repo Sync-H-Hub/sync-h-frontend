ff6ab40f5e42a3dc6020f64730a7aa3bc2778ef9
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-26 04:12:16 -0300

    feat(Tailwind)

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,7 +9,8 @@ export default {
       colors: {
         primary: "rgba(39, 251, 233, 1)",
         background: "rgba(26, 24, 27, 1)",
-        graphite: "rgba(32, 30, 34, 1)"
+        graphite: "rgba(32, 30, 34, 1)",
+        charcoal: "rgba(36, 34, 38, 1)",
       },
       backgroundImage: {
         'primary-gradient': 'linear-gradient(90deg, rgba(30, 214, 255, 1) 0%, rgba(40, 255, 229, 1) 100%)',
