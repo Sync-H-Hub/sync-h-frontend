1fb149ff2d7a1a9f4fe095c977cad40f44fd2204
joaogabrielvianna <joaogabrielvianna05@gmail.com>
2024-10-26 04:12:33 -0300

    feat(Model): Criei um Model para as Maos

---
+++ src/models/handModel.tsx
@@ -0,0 +1,8 @@
+const handsModel = {
+  hand_L: new URL('../assets/hands/hand_L.svg', import.meta.url).href,
+  hand_Rock: new URL('../assets/hands/hand_Rock.svg', import.meta.url).href,
+  hand_Paz: new URL('../assets/hands/hand_Paz.svg', import.meta.url).href,
+  hand_Cha: new URL('../assets/hands/hand_Cha.svg', import.meta.url).href,
+};
+
+export default handsModel;
